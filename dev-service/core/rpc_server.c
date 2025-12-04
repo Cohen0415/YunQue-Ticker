c72bbe09754bb47b3e79050a7ed0a56ef28f0480
Cohen0415 <1033878279@qq.com>
2025-12-04 08:52:20 +0000

    update: service: RPC Server 使用发送/接收队列处理数据

--- dev-service/core/rpc_server.c
+++ dev-service/core/rpc_server.c
@@ -1,5 +1,6 @@
 #include "rpc_server.h"
 #include "log.h"
+#include "queue.h"
 
 #include <sys/socket.h>
 #include <sys/un.h>
@@ -7,10 +8,19 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 #include <errno.h>
 
 static int g_listen_fd = -1;
 static int g_running = 0;
+
+static queue_t *recv_queue = NULL;
+static queue_t *send_queue = NULL;
+
+typedef struct {
+    int client_fd;
+    char *data;
+} rpc_msg_t;
 
 static char *read_all(int fd, int *out_len)
 {
@@ -52,6 +62,77 @@ static char *read_all(int fd, int *out_len)
     return buf;
 }
 
+// 接收线程
+static void *recv_thread(void *arg)
+{
+    rpc_on_message_cb cb = (rpc_on_message_cb)arg;
+
+    while(g_running)
+    {
+        int client_fd = accept(g_listen_fd, NULL, NULL);
+        if(client_fd < 0) continue;
+
+        int len = 0;
+        char *data = read_all(client_fd, &len);
+        if(data && len > 0)
+        {
+            rpc_msg_t *msg = malloc(sizeof(rpc_msg_t));
+            msg->client_fd = client_fd;
+            msg->data = data;
+            queue_push(recv_queue, msg); // 放到接收队列
+        } 
+        else 
+        {
+            close(client_fd);
+        }
+    }
+
+    return NULL;
+}
+
+// 处理线程
+static void *process_thread(void *arg)
+{
+    rpc_on_message_cb cb = (rpc_on_message_cb)arg;
+
+    while(g_running)
+    {
+        rpc_msg_t *msg = (rpc_msg_t*)queue_pop(recv_queue);
+        if(!msg) 
+            continue;
+
+        // 执行回调
+        cb(msg->client_fd, &(msg->data), strlen(msg->data));
+
+        // 处理完的数据直接入发送队列（这里可包装成响应）
+        queue_push(send_queue, msg); 
+        // 注意：发送后释放 msg 以及 msg->data 在发送线程完成
+    }
+
+    return NULL;
+}
+
+// 发送线程
+static void *send_thread(void *arg)
+{
+    (void)arg;
+
+    while(g_running)
+    {
+        rpc_msg_t *msg = (rpc_msg_t*)queue_pop(send_queue);
+        if(!msg) 
+            continue;
+
+        LOGD("send response to client_fd=%d: %s\n", msg->client_fd, msg->data);
+        write(msg->client_fd, msg->data, strlen(msg->data));
+        close(msg->client_fd);
+        free(msg->data);
+        free(msg);
+    }
+
+    return NULL;
+}
+
 int rpc_server_init_uds(const char *path)
 {
     unlink(path);
@@ -78,31 +159,24 @@ int rpc_server_init_uds(const char *path)
         return -1;
     }
 
+    recv_queue = queue_create();
+    send_queue = queue_create();
+
     g_running = 1;
     return 0;
 }
 
 void rpc_server_run(rpc_on_message_cb cb)
 {
-    if (g_listen_fd < 0 || !cb)
-        return;
+    pthread_t tid_recv, tid_process, tid_send;
 
-    while(g_running)
-    {
-        int client_fd = accept(g_listen_fd, NULL, NULL);
-        if(client_fd < 0) 
-            continue;
+    pthread_create(&tid_recv, NULL, recv_thread, cb);
+    pthread_create(&tid_process, NULL, process_thread, cb);
+    pthread_create(&tid_send, NULL, send_thread, NULL);
 
-        int len = 0;
-        char *data = read_all(client_fd, &len);
-        if(data && len > 0)
-        {
-            cb(client_fd, data, len);
-            free(data);
-        }
-
-        close(client_fd);
-    }
+    pthread_join(tid_recv, NULL);
+    pthread_join(tid_process, NULL);
+    pthread_join(tid_send, NULL);
 }
 
 void rpc_server_stop(void)
@@ -113,16 +187,9 @@ void rpc_server_stop(void)
         close(g_listen_fd);
         g_listen_fd = -1;
     }
-}
 
-int rpc_server_send_response(int client_fd, const char *data)
-{
-    if (!data)
-    {
-        LOGE("rpc_server_send_response: data is NULL\n");
-        return -1;
-    }
-
-    int sent = write(client_fd, data, strlen(data));
-    return sent;
+    if(recv_queue) 
+        queue_destroy(recv_queue);
+    if(send_queue) 
+        queue_destroy(send_queue);
 }
--- dev-service/core/rpc_server.h
+++ dev-service/core/rpc_server.h
@@ -1,11 +1,10 @@
 #ifndef RPC_SERVER_H
 #define RPC_SERVER_H
 
-typedef void (*rpc_on_message_cb)(int client_fd, const char *data, int len);
+typedef void (*rpc_on_message_cb)(int client_fd, char **data, int len);
 
 int rpc_server_init_uds(const char *path);
 void rpc_server_run(rpc_on_message_cb cb);
 void rpc_server_stop(void);
-int rpc_server_send_response(int client_fd, const char *data);
 
 #endif // RPC_SERVER_H
