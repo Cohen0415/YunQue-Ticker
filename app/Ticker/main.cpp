0f57ccebfe15c33ab338823285a0afb18c3f344b
Cohen0415 <1033878279@qq.com>
2026-01-06 14:54:00 +0800

    update: app: 优先执行页面初始化，减少桌面进入时间

--- app/Ticker/main.cpp
+++ app/Ticker/main.cpp
@@ -15,27 +15,28 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     Widget w;
 
+    // 初始化 AppContext
+#if defined(Q_OS_LINUX) && defined(Q_PROCESSOR_X86_64)
+
+#else
+    if (AppContext::getInstance()->init() != 0)
+    {
+        LOG_ERROR("Failed to initialize application context.");
+        return -1;
+    }
+#endif
+
+    // 初始化 Widget
+    w.init();
+    w.show();
+
     // 打印版本号
     LOG_DEBUG("App Version: %s", APP_GIT_VERSION);
 
-    // 打印系统运行至今的时间，以此判断设备从上电到进入桌面花了多长时间
-    QFile file("/proc/uptime");
-    if (file.open(QIODevice::ReadOnly | QIODevice::Text))
-    {
-        QByteArray line = file.readLine();
-        QList<QByteArray> parts = line.split(' ');
-        if (parts.size() >= 1)
-        {
-            double uptimeSeconds = parts[0].toDouble();
-            LOG_DEBUG("System Uptime : %d s", uptimeSeconds);
-        }
-        file.close();
-    }
-
-    // 1、安装自定义日志系统
+    // 安装自定义日志系统
     installCustomLogger();
 
-    // 2、设置字体
+    // 设置字体
     int id = QFontDatabase::addApplicationFont(":/res/font/AlimamaShuHeiTi-Bold.ttf");
     if (id != -1)
     {
@@ -48,20 +49,5 @@ int main(int argc, char *argv[])
         LOG_DEBUG("Faild to load custom font.");
     }
 
-    // 3、初始化 AppContext
-#if defined(Q_OS_LINUX) && defined(Q_PROCESSOR_X86_64)
-
-#else
-    if (AppContext::getInstance()->init() != 0)
-    {
-        LOG_ERROR("Failed to initialize application context.");
-        return -1;
-    }
-#endif
-
-    // 4、初始化 Widget
-    w.init();
-
-    w.show();
     return app.exec();
 }
