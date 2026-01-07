eea2cd807cdba7a43d25082ed03e8b9a383de73d
Cohen0415 <1033878279@qq.com>
2026-01-08 00:28:49 +0800

    update: qt: 使用事件循环延迟执行机制，将耗时初始化延迟到事件循环中执行

--- app/Ticker/main.cpp
+++ app/Ticker/main.cpp
@@ -19,38 +19,42 @@ int main(int argc, char *argv[])
     w.init0();
     w.show();
 
-    // 打印版本号
-    LOG_DEBUG("App Version: %s", APP_GIT_VERSION);
+    QTimer::singleShot(0, [&]() {
 
-    // 安装自定义日志系统
-    installCustomLogger();
+        LOG_DEBUG("Starting application initialization...");
 
-    // 设置字体
-    int id = QFontDatabase::addApplicationFont(":/res/font/AlimamaShuHeiTi-Bold.ttf");
-    if (id != -1)
-    {
-        QString family = QFontDatabase::applicationFontFamilies(id).at(0);
-        QFont font(family);
-        app.setFont(font);
-    }
-    else
-    {
-        LOG_DEBUG("Faild to load custom font.");
-    }
-
-    // 初始化 AppContext
+        // 初始化 AppContext
 #if defined(Q_OS_LINUX) && defined(Q_PROCESSOR_X86_64)
 
 #else
-    if (AppContext::getInstance()->init() != 0)
-    {
-        LOG_ERROR("Failed to initialize application context.");
-        return -1;
-    }
+        if (AppContext::getInstance()->init() != 0)
+        {
+            LOG_ERROR("Failed to initialize application context.");
+            return -1;
+        }
 #endif
 
-    // 初始化 Widget
-    w.init();
+        // 初始化 Widget
+        w.init();
 
+        // 安装自定义日志系统
+        installCustomLogger();
+
+        // 设置字体
+        int id = QFontDatabase::addApplicationFont(":/res/font/AlimamaShuHeiTi-Bold.ttf");
+        if (id != -1)
+        {
+            QString family = QFontDatabase::applicationFontFamilies(id).at(0);
+            QFont font(family);
+            app.setFont(font);
+        }
+        else
+        {
+            LOG_DEBUG("Faild to load custom font.");
+        }
+    });
+
+    // 打印版本号
+    LOG_DEBUG("App Version: %s", APP_GIT_VERSION);
     return app.exec();
 }
